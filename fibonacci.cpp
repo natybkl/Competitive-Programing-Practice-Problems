864aae9864e6055036860bff5c9ce28cc8aa8734
Natnael bekele <62074050+natybkl@users.noreply.github.com>
2021-01-13 07:21:04 -0800

    Create fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,41 @@
+// using iteration
+
+class Solution {
+public:
+    int fib(int n) {
+        
+        int sum,a,b;
+        
+        stack<int> s;
+        
+        s.push(0);
+        
+        for(int i=0; i<=n; i++) { 
+            
+        if(i==1){ sum=1; s.push(sum);  }    
+            
+           else if(i>=2) {  a=s.top(); s.pop(); b=s.top(); sum=a+b; s.push(a); s.push(sum); } 
+            
+        }
+        
+        return s.top();
+        
+        }
+        
+    
+};
+
+
+
+// with recursion
+
+class Solution {
+public:
+    int fib(int n) {
+    
+    
+        }
+        
+        
+    
+};
