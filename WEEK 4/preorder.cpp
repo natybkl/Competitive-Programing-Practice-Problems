6f334ee32c9406367f4bb1d72e54cdaa47b43511
Natnael bekele <62074050+natybkl@users.noreply.github.com>
2021-01-30 05:27:55 -0800

    Update preorder.cpp

--- WEEK 4/preorder.cpp
+++ WEEK 4/preorder.cpp
@@ -1,4 +1,4 @@
-
+//recursive solution
 class Solution {
 public:
     vector<int> arr;
@@ -10,6 +10,46 @@ public:
         pre(node->right);
         
     }
+    vector<int> preorderTraversal(TreeNode* root) {
+        
+        if(root == NULL) return arr;
+        pre(root);
+        return arr;
+        
+    }
+};
+
+//itrative solution
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> arr;
+    void pre(TreeNode* root) {
+        
+     stack<TreeNode*> s;
+     s.push(root);
+        
+       while(s.empty()!=true){
+            TreeNode* node=s.top();
+            s.pop();
+            arr.push_back(node->val);
+            if(node->right) s.push(node->right);
+            if(node->left) s.push(node->left);
+            
+        }
+        
+    }
+    
     vector<int> preorderTraversal(TreeNode* root) {
         
         if(root == NULL) return arr;
