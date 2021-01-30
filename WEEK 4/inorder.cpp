55f4246551752956cf78fe5fab1d251b9f53d26b
Natnael bekele <62074050+natybkl@users.noreply.github.com>
2021-01-30 06:03:39 -0800

    Update inorder.cpp

--- WEEK 4/inorder.cpp
+++ WEEK 4/inorder.cpp
@@ -1,3 +1,4 @@
+// recursive solution
 class Solution {
 public:
     vector<int> arr;
@@ -15,5 +16,49 @@ public:
         in(root);
         return arr;
         
+    }
+};
+
+// iterative solution
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    vector<int> v;
+    
+    void in(TreeNode* root) {
+        stack<TreeNode*> s;
+        while(!s.empty() || root != NULL){
+            
+            while(root != NULL){
+                
+                s.push(root);
+                root=root->left;
+            }
+            
+          TreeNode* temp=s.top();
+          s.pop();
+          v.push_back(temp->val);
+          root=temp->right;  
+            
+        }
+        
+    }
+    
+    vector<int> inorderTraversal(TreeNode* root) {
+        
+        if(root==NULL) return v;
+        in(root);
+        return v;
     }
 };
