555c6654e3f9c8546a7babefedfde6db7d035a86
Natnael bekele <62074050+natybkl@users.noreply.github.com>
2021-02-02 08:10:16 -0800

    Update validate binary tree.cpp

--- WEEK 4/validate binary tree.cpp
+++ WEEK 4/validate binary tree.cpp
@@ -24,3 +24,28 @@ public:
       return true;  
     }
 };
+
+
+//one round trip solution
+class Solution {
+public:
+    
+    bool valid(TreeNode* node,long long min,long long max){
+       
+        if(node->val <= min || node->val >= max) return false;
+        
+        if(node->left != NULL && !valid(node->left,min,node->val)) return false;
+        
+        if(node->right != NULL && !valid(node->right,node->val,max)) return false;
+        
+        return true;
+    }
+    
+    bool isValidBST(TreeNode* root) {
+        
+        if(root == NULL) return true;
+        
+        return valid(root,LLONG_MIN,LLONG_MAX);
+        
+    }
+};
