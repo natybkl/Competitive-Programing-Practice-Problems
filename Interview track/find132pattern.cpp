2ce06b4d91263bcf9af05d18e58f21e55bd1e7f6
Natnael bekele <62074050+natybkl@users.noreply.github.com>
2021-04-29 08:22:37 -0700

    Update find132pattern.cpp

--- Interview track/find132pattern.cpp
+++ Interview track/find132pattern.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
-    bool find132pattern(vector<int>& nums) {
-        vector<int> memo(nums.size());
-        stack<int> st;
-        memo[0] = num[0];
-        
-        for(int i = 1; i < nums.size(); i++){
-            memo[i] = min(memo[i-1], nums[i]);
-        }
-        
-        for(int i = nums.size() - 1; i >= 0; i--){
-            if(nums[i] > memo[i]){
-                while(!st.empty() && st.top() <= memo[i]){
-                    st.pop();
-                }
-                    
-                if(!st.empty() && st.top() < nums[i])
-                    return true;
-                
-                st.push(nums[i]);
+    
+    bool find132pattern(vector<int>& num) {
+        for(int i = 1; i < num.size() - 1; i++){
+            for(int j = i - 1; j >= 0; j--) {
+                if(num[j] < num[i-1]) 
+                    num[i-1] = num[j];
             }
-        }
+            
+            for(int j = i + 1; j < num.size(); j++) {
+                if(num[j] >  num[i-1] && num[j] < num[i])
+                    return true;
+            }
+        } 
         
-        return false;
+        return  false;
     }
 };
