ffb326ffc628d5f30d239e0dd66359cf0b1fa763
Natnael bekele <62074050+natybkl@users.noreply.github.com>
2021-04-30 13:26:47 -0700

    Create combinationSum4.cpp

---
+++ Interview track/combinationSum4.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public: 
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<double> memo(target + 1, 0);
+        memo[0] = 1;
+        
+        for (int i = 1; i <= target; i++) {
+            for (int j = 0; j < nums.size(); j++) {
+                if (i >= nums[j]) {
+                    memo[i] += memo[i - nums[j]];
+                }     
+            }
+        }
+        
+        return 1*memo[target];
+    }
+};
