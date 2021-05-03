d7a0de76fd0950ad6342a381d7ea31d9e26e2558
Natnael bekele <62074050+natybkl@users.noreply.github.com>
2021-05-03 15:01:42 -0700

    Update numberOfArithmeticSlices.cpp

--- Interview track/numberOfArithmeticSlices.cpp
+++ Interview track/numberOfArithmeticSlices.cpp
@@ -1,8 +1,10 @@
+
+// simple math solution
 class Solution {
 public:
     void solve(int& ans, int count){
 	    if(count < 3) 
-            return;
+ 		return;
 	    ans += ((count - 3 + 2)*(count - 3 + 1)) / 2;
     }
     
@@ -14,18 +16,36 @@ public:
         while(i + 1 < nums.size()){
             if(nums[i] - nums[i + 1] != diff || i + 2 == nums.size()){
                 if(nums[i] - nums[i + 1] == diff) count++;
-	            solve(ans, count);
-	            count = 1;
+	        solve(ans, count);
+	        count = 1;
                 diff = nums[i] - nums[i + 1];
                 if(i + 2 == nums.size()) break;
             } else {
                 count++;
                 i++;
-            }
-               
-        }
-        
-        
+            }    
+       }
+	    
         return ans;
     }
+};
+
+
+//dp solution
+class Solution{
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        if(nums.size() < 3) 
+            return 0;
+     
+        vector<int> dp(nums.size());
+        int  res = 0;
+        for(int i = 2; i < nums.size(); i++){
+	      if(nums[i] - nums[i-1] == nums[i-1] - nums[i-2])
+			dp[i] = dp[i-1] + 1; 
+              res += dp[i];
+        }
+	    
+        return  res;
+   }    
 };
