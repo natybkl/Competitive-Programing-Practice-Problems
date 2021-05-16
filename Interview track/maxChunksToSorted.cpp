3cccfc827a7d0a5b8faab3daa6826c1623b7339e
Natnael bekele <62074050+natybkl@users.noreply.github.com>
2021-05-16 08:44:09 -0700

    Update maxChunksToSorted.cpp

--- Interview track/maxChunksToSorted.cpp
+++ Interview track/maxChunksToSorted.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
-    	int count  = 0, i = 0, num = 0;
-	while(i < arr.size()) {
-		while(arr[i] != num) {
-			i++;
-			if(i == arr.size())
-				break;
-}
-count++;
-sort();
-num = ++i;
-}
+    	int count = 0, i;
+        unordered_map<int, int> idx;
+        
+        for(i = 0; i < arr.size(); i++) {
+            idx[arr[i]] = i;
+        }
+        
+        i = 0;
+        while(i < arr.size()) {
+            int cur = i;
+            while(i <= idx[cur]) {
+                idx[cur] = max(idx[cur], idx[i]);
+                i++;
+            }
+            count++;
+            i = idx[cur] + 1;
+        }
 
-return count;
+        return count;
     }
 };
+
