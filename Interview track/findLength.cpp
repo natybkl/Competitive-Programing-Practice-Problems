c522902a9f0a6f1e675b8a18e7c2f6fee03757d7
Natnael bekele <62074050+natybkl@users.noreply.github.com>
2021-07-08 19:15:42 +0300

    Update findLength.cpp

--- Interview track/findLength.cpp
+++ Interview track/findLength.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
-        int x=nums1.size();
-        int y=nums2.size();
-        int i,j,ct,ans=0;
+        int x = nums1.size();
+        int y = nums2.size();
+        int i,j,ct,ans = 0;
         
-        vector<vector<int>> res(y,vector<int>(x,0));
+        vector<vector<int>> res(y, vector<int>(x, 0));
         queue<pair<int,int>> pts;
 
-        for(int i=0; i < y; i++){
-            for(int j=0; j<x; j++){
-                if(nums2[i]==nums1[j]){
+        for(int i = 0; i < y; i++){
+            for(int j = 0; j < x; j++){
+                if(nums2[i] == nums1[j]){
                      res[i][j] = 1;        
                      pts.push({i,j});
                 }
             } 
         }   
 
-        if(pts.size()==0)
+        if(pts.size() == 0)
             return 0;
         
         while(!pts.empty()){
-            auto a=pts.front();
+            auto a = pts.front();
             pts.pop();
-            i=a.first;
-            j=a.second;
-            ct=1;
-            while(i+1<y&&j+1<x){
-                res[i][j]=0;
-                if(res[++i][++j]!=1)
+            i = a.first;
+            j = a.second;
+            ct = 1;
+            while(i+1 < y && j+1 < x){
+                res[i][j] = 0;
+                if(res[++i][++j] != 1)
                       break;      
                 ct++;
             }
 
-            ans=max(ans,ct);
+            ans = max(ans,ct);
         }
         
         return ans;
