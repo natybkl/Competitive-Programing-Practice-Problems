58b56694157379f679529161482cee313f0bb591
Natnael bekele <62074050+natybkl@users.noreply.github.com>
2021-07-17 08:19:00 +0300

    Update customSortString.cpp

--- Interview track/customSortString.cpp
+++ Interview track/customSortString.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
+        /* sorting with the help of lambda expression
+           that overloads the comparator in sort function
+           to sort character in "s" exactly as in the correct
+           position of "order" */
+
         sort(s.begin(), s.end(), [=](const char& ch1, const char& ch2){ return order.find(ch1) < order.find(ch2);}); 
         return s;
     }
