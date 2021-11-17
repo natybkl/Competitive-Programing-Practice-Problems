ce5698a57ff9e99611a48a7845f913a33de0a533
Natnael bekele <62074050+natybkl@users.noreply.github.com>
2021-11-17 10:08:49 +0300

    Create maxRepeating.cpp

---
+++ Interview track/maxRepeating.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxRepeating(string sequence, string word) {
+        int cnt = 0; 
+        string tmp = word;
+        while(sequence.find(tmp) != string::npos){
+            cnt++;
+            tmp += word;
+        }
+        
+        return cnt;
+    }
+};
